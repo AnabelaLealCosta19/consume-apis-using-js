c16b14848cb4b4060b44a1cbf4f0e14dcc177613
AnabelaLealCosta19 <anabela.leal.costa@gmail.com>
2020-07-18 22:25:04 +0000

    Add buttons that request data through APIs

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-let xhr = new XMLHttpRequest();
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
+
+
